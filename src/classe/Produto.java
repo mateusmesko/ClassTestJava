d9f04da444b3170b4aa5a5f67e3ea9bc8f61c487
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-10-09 22:57:57 -0300

    Add files via upload

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -1,8 +1,21 @@
 package classe;
 
 public class Produto {
+	String nome;
+	double preco;
+	static double desconto;
 	
-	public String nome;
-	public double preco;
-	public double desconto;
+	Produto(){
+		
+	}
+	
+	Produto(String nome, double preco){
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	double precoComDesconto() {
+		return preco - preco * desconto;
+	}
+
 }
