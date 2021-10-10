c04537d69624f8c17df61f7b41b7348f8dc9fecc
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-10-09 22:57:32 -0300

    Add files via upload

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -1,7 +1,23 @@
 package classe;
 
 public class Data {
-	int DD;
-	int MM;
-	int AAAA;
+		
+		int dia;
+	    int mes;
+	    int ano;
+	    
+	    Data() {
+	    	this(1, 1, 2017);
+	    }
+	     
+	    Data(int dia, int mes, int ano) {
+	    	this.dia = dia;
+	    	this.mes = mes;
+	    	this.ano = ano;
+	    }
+	    
+	    String formatar() {
+	    	return String.format("%d/%d/%d", dia, mes, ano);
+	    }
+	    
 }
