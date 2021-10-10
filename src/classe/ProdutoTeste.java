fc5a2024d06270bc083903b888c4b8e4366ff6ff
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-10-09 22:58:05 -0300

    Add files via upload

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -1,28 +1,17 @@
 package classe;
 
 public class ProdutoTeste {
+
 	public static void main(String[] args) {
+		Produto produto1 = new Produto();
+		produto1.nome = "Notebook Acer 15' 8GB";
+		produto1.preco = 2300;
 		
-		Produto p1= new Produto();
-		p1.nome="Notebook";
-		p1.preco=4356.89;
-		p1.desconto=0.25;
+		Produto produto2 = new Produto("Caneta BIC Preata", 2.83);
 		
-		Produto p2 = new Produto();
-		p2.nome="caneta preta";
-		p2.preco=2.56;
-		p2.desconto=0.29;
+		Produto.desconto = 0.05;
 		
-		System.out.println(p1.nome);
-		System.out.println(p1.preco);
-		System.out.println(p1.desconto);
-		System.out.println(p2.nome);
-		System.out.println(p2.preco);
-		System.out.println(p2.desconto);
-		
-		double precoFinal1=p1.preco*(1-p1.desconto);
-		double precoFinal2=p2.preco*(1-p2.desconto);
-		double mediaCarrinho=(precoFinal1+precoFinal2)/2;
-		System.out.printf("media do carrinho é = R$%.2f",mediaCarrinho);
+		System.out.println(produto1.precoComDesconto());
+		System.out.println(produto2.precoComDesconto());
 	}
 }
