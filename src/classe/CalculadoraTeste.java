93005317cae49f7ab1c44e05c4e87ea5c1825a91
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-11-10 22:59:01 -0300

    Update CalculadoraTeste.java

--- src/classe/CalculadoraTeste.java
+++ src/classe/CalculadoraTeste.java
@@ -6,7 +6,7 @@ public class CalculadoraTeste {
 		Calculadora calc = new Calculadora();
 		
 		System.out.println(calc.somar(2.3, 4.5));
-		
+		 
 		calc.acrescentar(10);
 		System.out.println(calc.obterResultado());
 		
